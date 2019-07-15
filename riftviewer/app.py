1e32aecf25ad5c89263cc75df734cb53647aafc0
Kevin Jiao <kevin.jiao@berkeley.edu>
2019-07-14 23:31:04 -0700

    Add esports_api module
    This adds the esports_api module, to consolidate the various api calls
    to the lol esports api.

--- riftviewer/app.py
+++ riftviewer/app.py
@@ -1,12 +1,21 @@
 """Main module"""
 import npyscreen
 
+from riftviewer.esports_api import League
+
 
 class ScheduleForm(npyscreen.Form):
+    def __init__(self, *args, **kwargs):
+        """Retrieve the list of games"""
+        super().__init__(*args, **kwargs)
+        self.league = League()
+
     def afterEditing(self):
         self.parentApp.setNextForm(None)
 
     def create(self):
+        #TODO Hook up the games to the picker
+        games = self.league.get_games()
         self.game = self.add(
             npyscreen.TitleSelectOne, scroll_exit=True, name='Upcoming matches',
             values=['C9 vs G2', 'TL vs FNC', 'TSM vs OG'])
