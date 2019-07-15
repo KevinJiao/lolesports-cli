fb6c3f173e31d693ddbdc82e4f943d498d6745af
Kevin Jiao <kevin.jiao@berkeley.edu>
2019-07-14 21:22:09 -0700

    linting fixes

--- riftviewer/app.py
+++ riftviewer/app.py
@@ -1,17 +1,21 @@
 """Main module"""
 import npyscreen
 
+
 class ScheduleForm(npyscreen.Form):
     def afterEditing(self):
         self.parentApp.setNextForm(None)
 
     def create(self):
         self.game = self.add(
-            npyscreen.TitleSelectOne, scroll_exit=True, name='Upcoming matches', values=['C9 vs G2', 'TL vs FNC', 'TSM vs OG'])
+            npyscreen.TitleSelectOne, scroll_exit=True, name='Upcoming matches',
+            values=['C9 vs G2', 'TL vs FNC', 'TSM vs OG'])
+
 
 class RiftviewerApp(npyscreen.NPSAppManaged):
     def onStart(self):
         self.addForm('MAIN', ScheduleForm, name='Schedule')
+
 
 def main():
     RiftviewerApp().run()
